0be4a69c6c3c848d7aadf4e027a2fe54f1ff0687
Craig Hurley <craig@thehurley.com>
2026-03-03 16:17:58 +1300

    Use official plugins for certain skills

--- install.sh
+++ install.sh
@@ -3,29 +3,37 @@
 # Start
 echo "$(basename "$0") starting."
 
-# Update go packages
+# Install/update go packages
+echo "go packages..."
 go install github.com/sammcj/mcp-devtools@HEAD
 go install github.com/sammcj/run_silent@HEAD
 go install golang.org/x/tools/gopls@latest
 
-# Update npm
+# Install/update npm
+echo "npm..."
 npm install -g npm
 npm install -g --omit=dev pyright
 npm install -g --omit=dev typescript-language-server typescript
 
-# # Update claude
+# Update claude
+echo "Update claude..."
 claude update
 
-# Plugins
+# Install/update claude code plugins
+echo "claude plugins..."
 claude plugin marketplace update claude-plugins-official
 claude plugin install claude-md-management
+claude plugin install code-review
+claude plugin install code-simplifier
 claude plugin install gopls-lsp
 claude plugin install pyright-lsp
 claude plugin install skill-creator
+claude plugin install superpowers
 claude plugin install swift-lsp
 claude plugin install typescript-lsp
 
-# MCP servers
+# Add MCP servers
+echo "claude MCP servers..."
 claude mcp add --transport http --scope user excalidraw https://mcp.excalidraw.com
 claude mcp add --transport stdio --scope user mcp-devtools --env ENABLE_ADDITIONAL_TOOLS="aws_documentation,code_skim,excel,murican_to_english,process_document,terraform_documentation" -- $HOME/go/bin/mcp-devtools
 
